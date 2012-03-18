74b980fd4b8553f29b346c551046d48c3aa3cc1f
John Ryan <john.p.ryan4@gmail.com>
2012-03-18 10:12:42 -0500

    added work directory and control base module

---
+++ code/control.vhd
@@ -0,0 +1,35 @@
+library IEEE;
+use IEEE.std_logic_1164.ALL;
+use IEEE.numeric_std.ALL;
+
+--------------------------
+-- Control Logic Module --
+--------------------------
+-- John Ryan and Juan Mozqueda
+
+entity control is
+  port(
+    );
+end control;
+
+architecture structure of control is
+----------------
+-- Components --
+----------------
+  component alu_32bit
+    port( ian_op_sel : in std_logic_vector(2 downto 0);
+          ian_A : in std_logic_vector(31 downto 0);
+          ian_B : in std_logic_vector(31 downto 0);
+          oan_result : out std_logic_vector(31 downto 0);
+          oan_overflow : out std_logic );
+  end component;
+
+-------------
+-- Signals --
+-------------
+
+---------------
+-- Structure --
+---------------
+
+end structure;
