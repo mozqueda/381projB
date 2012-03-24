0e124f5bca9943ad466f5fe2aacd6d6ce1e2b51f
John Ryan <john.p.ryan4@gmail.com>
2012-03-24 16:53:41 -0500

    processor tb is ready to write

--- code/tb_processor.vhd
+++ code/tb_processor.vhd
@@ -2,31 +2,44 @@ library IEEE;
 use IEEE.std_logic_1164.all;
 
 entity tb_processor is
-
+  generic(gCLK_HPER : time :=50ns);
 end tb_processor;
 
 architecture behavior of tb_processor is
 
+  constant cCLK_PER : time := gCLK_HPER * 2;
   component processor
     port(p_CLK : in std_logic;
          reset : in std_logic);
   end component;       
-  
-  signal t_CLK : std_logic;
-  
-  
+
+  signal s_clk      : std_logic;
+
   begin
-    DUT: processor
-      port map(p_CLK => t_CLK,
+      DUT: processor
+      port map(p_CLK => s_clk,
                reset => '0');
-                          
-    begin process
-      
+
+             
+                      
+  -- This process sets the clock value (low for gCLK_HPER, then high
+  -- for gCLK_HPER). Absent a "wait" command, processes restart 
+  -- at the beginning once they have reached the final statement.
+  P_CLK: process
+    begin
+      s_CLK <= '0';
+      wait for gCLK_HPER;
+      s_CLK <= '1';
+      wait for gCLK_HPER;
+  end process;    
+  
+  
+  
+  processor_tb: process
+    begin
     
-    
-    
-    
-    
-    end process;
+  end process;
+
+
 end behavior;
      
