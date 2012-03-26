943884e5ac5365495f87c37e5bf92758f8dc8ff2
mozqueda <mozqueda@iastate.edu>
2012-03-26 15:53:04 -0500

    Added two signals just for testing

--- code/control.vhd
+++ code/control.vhd
@@ -50,6 +50,13 @@ begin
   opcode <= instruction(31 downto 26);
   funct_code <= instruction(5 downto 0);
   
+    -- the following statements aren't correct right now --
+  sHelper <= "1010100-0---0010-----00--0--0" when opcode = "000000" AND funct_code = "100000" else
+             "1011100-0---0000-----00--0--0" when opcode = "001000" else
+             "1011100-00--0010-----00--1--0" when opcode = "001001" else
+             "1011100-00--0000-----00--1--0' when opcode = "000000" AND funct_code = "100001" else
+             "00000000000000000000000000000";
+
   alu_op <= sHelper(28 downto 26);
   alu_src <= sHelper(25);
   reg_write <= sHelper(24);
